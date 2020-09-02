5b74565d5ca8c69fd685da46a40669cbc945f562
Васильев Дмитрий <lotus33497@yandex.ru>
2020-09-02 20:24:27 +0700

    It can count

--- src/CoffeeMachine.java
+++ src/CoffeeMachine.java
@@ -1,11 +1,33 @@
+import java.util.Scanner;
+
 public class CoffeeMachine {
     public static void main(String[] args) {
-        System.out.println("Starting to make a coffee\n" +
-                "Grinding coffee beans\n" +
-                "Boiling water\n" +
-                "Mixing boiled water with crushed coffee beans\n" +
-                "Pouring coffee into the cup\n" +
-                "Pouring some milk into the cup\n" +
-                "Coffee is ready!\n");
+        Scanner scanner = new Scanner(System.in);
+        int milkForCup = 50;
+        int waterForCup = 200;
+        int beansForCup = 15;
+        System.out.println("Write how many ml of water the coffee machine has:");
+        int amountOfWater = scanner.nextInt();
+        System.out.println("Write how many ml of milk the coffee machine has:");
+        int amountOfMilk = scanner.nextInt();
+        System.out.println("Write how many grams of coffee beans the coffee machine has:");
+        int amountOfBeans = scanner.nextInt();
+        System.out.println("Write how many cups of coffee you will need:");
+        int numberOfCups = scanner.nextInt();
+        int[] supplies = {amountOfWater, amountOfMilk, amountOfBeans, numberOfCups};
+        int[] forCup = {waterForCup, milkForCup, beansForCup, amountOfWater / waterForCup};
+
+        for (int i = 0; i < 3; i++) {
+            if (supplies[i] / forCup[i] < forCup[3]) {
+                forCup[3] = supplies[i] / forCup[i];
+            }
+        }
+        if (supplies[3] == forCup[3]) {
+            System.out.println("Yes, I can make that amount of coffee");
+        } else if (supplies[3] > forCup[3]) {
+            System.out.printf("No, I can make only %d cup(s) of coffee", forCup[3]);
+        } else {
+            System.out.printf("Yes, I can make that amount of coffee (and even %d more than that)", forCup[3] - supplies[3]);
+        }
     }
 }
